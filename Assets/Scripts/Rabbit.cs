aa98a74e9f4a96ed7b3cd96be11e4a344174a993
Molom <venmolom@gmail.com>
2020-05-04 19:41:00 +0200

    prototype 1.0

---
+++ Assets/Scripts/Rabbit.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Rabbit : MonoBehaviour
+{
+    [SerializeField]
+    private float openDuration;
+    [SerializeField]
+    private Color openColor;
+    [SerializeField]
+    private Color defaultColor;
+    [SerializeField]
+    private UnityEvent onCollideWhileOpen;
+    [SerializeField]
+    private UnityEvent onCollideWhileClosed;
+
+    private Renderer renderer;
+
+    private bool isOpen = false;
+
+    private void Start()
+    {
+        renderer = GetComponent<Renderer>();
+        renderer.material.color = defaultColor;
+    }
+
+    public void Open()
+    {
+        if (!isOpen)
+        {
+            isOpen = true;
+            StartCoroutine(OpenCoroutine());
+        }
+    }
+
+    private IEnumerator OpenCoroutine()
+    {
+        renderer.material.color = openColor;
+
+        yield return new WaitForSeconds(openDuration);
+
+        renderer.material.color = defaultColor;
+        isOpen = false;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Destroy(collision.gameObject);
+        if(isOpen)
+        {
+            onCollideWhileOpen.Invoke();
+        }
+        else
+        {
+            onCollideWhileClosed.Invoke();
+        }
+    }
+}
