aecb590b295ea221d49346b489faff3877dc0783
Filip Hausman <filip.hausman@gmail.com>
2020-05-05 22:47:12 +0200

    pepperoncino orbiting around pizza

---
+++ Assets/Scripts/Pepperoncino.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Pepperoncino : MonoBehaviour
+{
+    [Range(0.0f, 5.0f)]
+    public float radius;
+    public Transform obj;
+
+    void Update()
+    {
+        obj.localPosition = new Vector3(radius, 0.0f, 0.0f);
+    }
+
+    public void OnPepperoncinoShot()
+    {
+        //todo: naive reloding, change
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
