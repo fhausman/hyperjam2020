94587e5eac2ffb37227f5b1a62b5a180c74e8d00
Molom <venmolom@gmail.com>
2020-05-05 23:46:52 +0200

    Added ingame gui, pause screen, lost screen

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,37 +1,108 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour
 {
     [SerializeField]
-    private Canvas pauseScreen;
+    private GameObject pauseScreen;
+    [SerializeField]
+    private GameObject lostScreen;
+    [SerializeField]
+    private Text lostScoreText;
+    [SerializeField]
+    private Image healthBar;
+    [SerializeField]
+    private Text scoreText;
+    [SerializeField]
+    private Toggle soundToggle;
 
     private bool isPaused = false;
+    private int score = 0;
+    private Vector2 healthBarSize;
+
+    private void Awake()
+    {
+        if (!PlayerPrefs.HasKey("music"))
+        {
+            PlayerPrefs.SetInt("music", 1);
+            soundToggle.isOn = true;
+            AudioListener.volume = 1;
+            PlayerPrefs.Save();
+        }
+        else
+        {
+            if (PlayerPrefs.GetInt("music") == 0)
+            {
+                soundToggle.isOn = false;
+                AudioListener.volume = 0;
+            }
+            else
+            {
+                soundToggle.isOn = true;
+                AudioListener.volume = 1;
+            }
+        }
+    }
+
+    private void Start()
+    {
+        scoreText.text = score.ToString();
+        healthBarSize = healthBar.rectTransform.sizeDelta;
+    }
 
     public void Pause()
     {
         if (isPaused)
         {
             Time.timeScale = 1;
-
-            if (pauseScreen)
-            {
-                pauseScreen.enabled = false;
-            }
-
+            pauseScreen.SetActive(false);
             isPaused = false;
         }
         else
         {
             Time.timeScale = 0;
-
-            if (pauseScreen)
-            {
-                pauseScreen.enabled = true;
-            }
-
+            pauseScreen.SetActive(true);
             isPaused = true;
         }
+    }
+
+    public void ModifyHealthBar(float ratio)
+    {
+        healthBar.rectTransform.sizeDelta = new Vector2(healthBarSize.x * ratio, healthBarSize.y);
+    }
+
+    public void IncreaseScore(int add)
+    {
+        score += add;
+        scoreText.text = score.ToString();
+    }
+
+    public void ToggleSound()
+    {
+        if (soundToggle.isOn)
+        {
+            PlayerPrefs.SetInt("music", 1);
+            AudioListener.volume = 1;
+        }
+        else
+        {
+            PlayerPrefs.SetInt("music", 0);
+            AudioListener.volume = 0;
+        }
+        PlayerPrefs.Save();
+    }
+
+    public void Lost()
+    {
+        Time.timeScale = 0;
+        lostScreen.SetActive(true);
+        lostScoreText.text = "Your score: \n" + score;
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        Time.timeScale = 1;
     }
 }
