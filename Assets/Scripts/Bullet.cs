2e170a8f00897088c212769329b47a57e379f4b8
Molom <venmolom@gmail.com>
2020-05-06 15:49:56 +0200

    Added bullet speed controll and slowing angle mechanic

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    [SerializeField]
+    private float bulletInitialSpeed;
+    [SerializeField]
+    private float bulletSpeedDelta;
+
+    private Rigidbody2D rigidbody2D;
+
+    private void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        rigidbody2D.AddForce(new Vector2(0, bulletInitialSpeed));
+    }
+
+    private void FixedUpdate()
+    {
+        rigidbody2D.AddForce(new Vector2(0, bulletSpeedDelta) * Time.deltaTime);
+    }
+}
