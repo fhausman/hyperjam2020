b70dcebf3f3021cde52f789b8884934513877a5c
Molom <venmolom@gmail.com>
2020-05-07 18:32:21 +0200

    Added backgroundmusic

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+    private void Awake()
+    {
+        if (GameObject.FindGameObjectsWithTag("Music")?.Length > 1)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(this.gameObject);
+        }
+    }
+}
